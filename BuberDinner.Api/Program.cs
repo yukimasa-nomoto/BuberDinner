b0c733a844a83b9bbb3b38f1b6a1922b0edd5455
野元之雅 <yukimasa_nomoto@csc1980.co.jp>
2022-12-08 17:27:33 +0900

    12/8終了

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,25 +1,35 @@
+using BuberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    builder.Services.AddScoped<IAuthenticationService , AuthenticationService>();
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    //builder.Services.AddEndpointsApiExplorer();
+    //builder.Services.AddSwaggerGen();
+
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var app = builder.Build();
+{
+    //// Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseSwagger();
+    //    app.UseSwaggerUI();
+    //}
 
-app.MapControllers();
+    app.UseHttpsRedirection();
 
-app.Run();
+    //app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,13 @@
+2022/12/09
+次は、ApplicationLayerでAuthを行う
+（31:35）
+
 2022/12/08
 ApiとContractsが一緒（PresentationLayer）
 	ContractsはAppicationを参照しない
+
+EndPointとは、apiを呼ぶこと？
+IDとトークンを返すように作成
+
+apiで、そのまま返すのは確認済み
 
